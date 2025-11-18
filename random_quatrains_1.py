d32a2f0e5609a43c057ad3b228a09fea9afd1356
sohanyan-krisp <sohanyan@krisp.ai>
2025-11-18 18:21:07 +0400

    delet s

--- random_quatrains_1.py
+++ random_quatrains_1.py
@@ -8,7 +8,7 @@ import random
 # stanzas from "Mad Girl's Love Song"
 stanzas = [
     [
-        "I shut my eyes and all the world drops dead;",
+        "I shut my eye and all the world drop dead;",
         "I lift my lids and all is born again.",
         "(I think I made you up inside my head.)",
     
